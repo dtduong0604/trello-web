143cb0719717de0c22c46b9d0f1b7592f33afcb2
Do Tung Duong <2251061751@e.tlu.edu.vn>
2026-01-13 23:46:35 +0700

    updateENV

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,13 @@
-export const API_ROOT = 'https://trello-api-8nf5.onrender.com'
+let apiRoot =''
+
+console.log(import.meta.env)
+console.log(process.env)
+
+if(process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'http://localhost:8017'
+}
+if(process.env.BUILD_MODE === 'production') {
+    apiRoot = 'https://trello-api-8nf5.onrender.com'
+}
+
+export const API_ROOT = apiRoot
--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import svrg from 'vite-plugin-svgr'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env
+  },
   plugins: [
     react(),
     svrg()
